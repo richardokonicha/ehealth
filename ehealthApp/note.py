20c27dc3dc0dbc1960613ce6f69178a0bc6f03d9
richardokonicha@gmail.com <reechee@r.e.e.c.h.e.e-PC>
2019-08-09 09:08:26 +0100

    still fresh

--- ehealthApp/note.py
+++ ehealthApp/note.py
@@ -1,4 +1,11 @@
 s = "Look right here "
 v = "I made you see this "
-def t(arg):
-  print(s, arg)
+
+
+def off(arg):
+    """Is he kissing her, i'm pissed off"""
+    print(f'arg = {arg}')
+
+
+def offme():
+    print("remi boy hejeh")
